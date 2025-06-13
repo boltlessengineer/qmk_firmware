9e34ef01e0889f9f42fcfb8bf45a94b050a5130e
Seongmin Lee <boltlessengineer@proton.me>
2025-06-13 20:25:59 +0900

    feat: add mousekey layer

--- keyboards/crkbd/keymaps/boltlessengineer/keymap.c
+++ keyboards/crkbd/keymaps/boltlessengineer/keymap.c
@@ -28,18 +28,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HOME_L RALT_T(KC_L)
 #define HOME_SC RCTL_T(KC_SCLN)
 
+#define MT_TAB LCTL_T(KC_TAB)
+
 #define GUI_ESC LGUI_T(KC_ESC)
 #define LT_SPC LT(1, KC_SPC)
-#define LT_TAB LT(1, KC_TAB)
+#define LT_TAB LT(4, KC_TAB)
 
 #define LT_ENT LT(2, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O, KC_BSLS, KC_BSPC,
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O, KC_BSLS, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,                         KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
+       MT_TAB,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,                         KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M,    KC_P, KC_RSFT, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -49,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_BSLS,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_LBRC, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, KC_COMM,  KC_DOT, KC_SLSH, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX,    KC_4,    KC_5,    KC_6, KC_MINS,                      XXXXXXX, KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______,    KC_1,    KC_2,    KC_3,  KC_GRV,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, _______,    KC_1,    KC_2,    KC_3,  KC_GRV,                      XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_RSFT, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           GUI_ESC,    KC_0,  LT_SPC,    _______, _______,   MO(3)
                                       //`--------------------------'  `--------------------------'
@@ -73,28 +75,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT,  KC_F12,   KC_F7,   KC_F8,   KC_F9, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_VOLU,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,  KC_F11,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_VOLD,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______,  KC_F10,   KC_F1,   KC_F2,   KC_F3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                            KC_F10,  KC_F11,  KC_F12,     LT_ENT,   MO(2), _______
                                       //`--------------------------'  `--------------------------'
-  )
+  ),
+
+    [4] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      _______, XXXXXXX, MS_BTN4, XXXXXXX, MS_BTN5, MS_WHLU,                      XXXXXXX, MS_BTN4, XXXXXXX, MS_BTN5, MS_WHLU, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, MS_WHLD,                      MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, MS_WHLD, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_LSFT, MS_BTN3, MS_BTN2, MS_BTN1, MS_WHLU,                      XXXXXXX, MS_BTN1, MS_BTN2, MS_BTN3, MS_WHLU, KC_RSFT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          GUI_ESC,  LT_SPC, _______,     LT_ENT,   MO(2),   MO(3)
+                                      //`--------------------------'  `--------------------------'
+  ),
 };
 
-enum combo_events {
-    KL_LANG,
-    SD_ESC,
+#define COMBO_LIST \
+    COMBO_X(KL_LANG, C(KC_SPC), HOME_K, HOME_L) \
+    COMBO_X(SD_ESC, KC_ESC, HOME_S, HOME_D)
+
+enum my_combos {
+#define COMBO_X(name, combos, ...) name,
+COMBO_LIST
+#undef COMBO_X
 };
 
-const uint16_t PROGMEM test_combo[] = {HOME_K, HOME_L, COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {HOME_S, HOME_D, COMBO_END};
+#define COMBO_X(name, combos, ...) const uint16_t PROGMEM name##_combo[] = {__VA_ARGS__, COMBO_END};
+COMBO_LIST
+#undef COMBO_X
 
 combo_t key_combos[] = {
-    [KL_LANG] = COMBO(test_combo, C(KC_SPC)),
-    [SD_ESC] = COMBO(sd_combo, KC_ESC),
+#define COMBO_X(name, combos, ...) [name] = COMBO(name##_combo, combos),
+COMBO_LIST
+#undef COMBO_X
 };
 
 #ifdef ENCODER_MAP_ENABLE
